5d17421df467b6be467ee7a8c8959701fe0bdee4
Siarheispivak <siarheispivak2@gmail.com>
2023-02-23 09:59:13 +0300

    feat: add typewriter by using hook React Simple Typewriter

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,9 +1,16 @@
 import React, {useState} from 'react';
 import style from './Welcome.module.css'
 import {Button} from "../elements/button/Button";
+import {useTypewriter,Cursor} from "react-simple-typewriter";
 
 export const Welcome = () => {
-
+    const [text] = useTypewriter({
+        words:[' Siarhei Spivak',' a Front-End Developer.'],
+        loop:0,
+        // typeSpeed:80,
+        // deleteSpeed:50,
+        // delaySpeed:1500
+    })
     return (
         <div className={style.welcomeSection}>
 {/*<BackgroundVideo/>*/}
@@ -12,7 +19,7 @@ export const Welcome = () => {
 
                     <div className={style.info}>
                         <span className={style.greeting}>Welcome</span>
-                        <div className={style.text}>I'm Siarhei Spivak.</div>
+                             <div className={style.text}>{`I\'m${text}`}<Cursor cursorColor={'#20c997'} cursorBlinking/></div>
                         <span className={style.address}>Based in Belarus,Minsk.</span>
                     </div>
                     <Button callBack={()=>{
@@ -22,30 +29,3 @@ export const Welcome = () => {
         </div>
     );
 };
-
-
-
-
-//
-// <div className={style.typewriter}>
-//     <span>Welcome</span>
-//     <h1>I'm Siarhei Spivak.</h1>
-//     <span>Based in Belarus,Minsk.</span>
-// </div>
-//
-
-// export const BackgroundVideo = () => {
-//     // const videoSource = 'http://www.w3schools.com/tags/movie.mp4';
-//     const videoSource = '../../assets/matrix.mp4';
-//
-//     return(
-//         <div>
-//                 <div className={style.Container}>
-//                     <video autoPlay loop muted className={style.Video}>
-//                     <source src={videoSource} type="video/mp4"/>
-//                     </video>
-//
-//                 </div>
-//         </div>
-//     )
-// }
